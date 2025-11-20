023f5ae086efd50262ba8619d7899260812a5f86
Raunit Raj <raunit.raj@pal.tech>
2025-11-20 15:45:49 +0530

    added initial data files

---
+++ models/STAGES/RAW/STG_RAW_PLAYERSEASON.sql
@@ -0,0 +1,15 @@
+{{ 
+    config(
+        materialized='table'
+    )
+}}
+
+WITH source_data AS (
+    SELECT
+        {{ dbt_utils.star(from=source('t20_database', 'playerseason')) }},
+        CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_
+    FROM {{ source('t20_database', 'playerseason') }}
+)
+
+SELECT *
+FROM source_data
