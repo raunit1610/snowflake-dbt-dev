fcf5827524bd2974502b4e755473ce383c467a09
Raunit Raj <raunitraj2018@gmail.com>
2025-12-24 13:53:05 +0530

    updated main merge config file

--- models/STAGES/RAW/STG_RAW_PLAYER.sql
+++ models/STAGES/RAW/STG_RAW_PLAYER.sql
@@ -13,7 +13,8 @@ WITH source_data AS (
         CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_2,
         CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_3,
         CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_4,
-        CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_5
+        CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_5,
+        CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_6
     FROM {{ source('t20_database', 'players') }}
 ),
 
@@ -27,7 +28,8 @@ deduped AS (
         MAX(_inserted_at_2) AS _inserted_at_2,
         MAX(_inserted_at_3) AS _inserted_at_3,
         MAX(_inserted_at_4) AS _inserted_at_4,
-        MAX(_inserted_at_5) AS _inserted_at_5
+        MAX(_inserted_at_5) AS _inserted_at_5,
+        MAX(_inserted_at_6) AS _inserted_at_6
     FROM source_data
     GROUP BY PLAYERID
 )
